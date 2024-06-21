c2e0af8f9b505b6901146d43a6a1d6272624c7e2
raj-Sunny <129651974+raj-Sunny@users.noreply.github.com>
2024-06-21 12:19:57 +0530

    Third Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const { PORT }=require('./config/envconfig.js')
+const bodyParser=require("body-parser");
 
 const setupAndStartServer=async()=>{
    const app=express()
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({extended:true}))
    app.listen(PORT,()=>{
     console.log(PORT)
       console.log(`server Start at port ${PORT}`)
